912824783f75aeff2417f564c9836a4cb8384a43
DevCHW <ggoma003@naver.com>
2023-06-18 06:52:07 +0900

    SwaggerConfig 파일 추가

---
+++ config/swagger/SwaggerConfig.java
@@ -0,0 +1,38 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final String API_NAME = "OOO Project REST API"; //API 문서 제목
+    private static final String API_VERSION = "0.0.1";  //API version
+    private static final String API_DESCRIPTION = "OOO Project API 명세서입니다."; //API 문서 설명
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .contact(new Contact("작성자 이름", "URL", "ggoma003@naver.com"))
+                .build();
+    }
+}
